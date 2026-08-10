4ed481b401be559c5b6ba93dca80097473d449eb
Stanley Nguyen <stan.ng66@gmail.com>
2026-08-10 18:48:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// ====================
+// Lab 10
+// Stanley Nguyen
+// Humber College
+// CPAN-144-RNA
+// Ronak Sheth
+// August 18, 2026
+// ====================
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lab 10</h1>
+        <h2>Stanley Nguyen</h2>
+        <h3>Practicing Vercel Deployment</h3>
       </header>
+
+      <div className="App-body">
+        <p>Hello, World</p>
+        <p>I am practicing using Vercel</p>
+      </div>
     </div>
   );
 }
